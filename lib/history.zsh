9f8a91ce7b7ca0d08d3bae158eb6528e4755e842
rwl <rwl@localhost>
2020-06-30 16:23:20 -0400

    updated

--- lib/history.zsh
+++ lib/history.zsh
@@ -29,8 +29,8 @@ esac
 
 ## History file configuration
 [ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-HISTSIZE=50000
-SAVEHIST=10000
+HISTSIZE=500000
+SAVEHIST=100000
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
